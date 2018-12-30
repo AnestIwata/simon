4fa5d68d589dd7f6f0234b360de30ccd3d20f397
Pawel P <pawelpelech@gmail.com>
2018-12-30 10:50:27 +0000

    Creating js variables necessary for program functionality

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//Creating necessary variabales for the program functionality
+let order = [];     //Order of flashing lights
+let userOrder =[];  //Order of user input
+let flash;  //Number of flashes appealing in the game
+let turn;   //Number of turns
+let good    //Checks if player have pushed correct buttons
+let gameTurn //Checks weather it's users turn or game's turn 
+let sequence //Checks wheather sequence input was correct
+let strict = false; //Checks if strict button was applied
+let sound = true; //Manages sounds in the game 
+let on = false; //Checks if power button was pressed
+let winner; //Checks if user have won the game 
