6ba395adfe86417e9e455b8903c005e69cf9f7c8
Pawel P <pawelpelech@gmail.com>
2018-12-30 12:04:16 +0000

    Added functionality of power button

--- js/index.js
+++ js/index.js
@@ -10,3 +10,34 @@ let strict = false; //Checks if strict button was applied
 let sound = true; //Manages sounds in the game 
 let on = false; //Checks if power button was pressed
 let winner; //Checks if user have won the game 
+
+//Referencing  all html elements in JavaScript
+
+const countTurn = document.querySelector("#turn");
+const topLeft = document.querySelector("#topleft");
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomleft");
+const bottomRight = document.querySelector("#bottomright");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+strictButton.addEventListener('click', (event) => {
+    if (strictButton.checked == true) {
+        strict = true; 
+        } else {
+        strict = false;
+        }
+})
+
+onButton.addEventListener('click', (event) => {
+  if (onButton.checked == true) {
+    on = true;
+    countTurn.innerHTML = "---";
+  } else {
+    on = false;
+    countTurn.innerHTML = "";
+    
+  }
+});
+
