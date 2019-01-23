91387a5090c9d4be6bd5d32b2fd28a645650410f
Pawel P <pawelpelech@gmail.com>
2019-01-23 17:34:18 +0000

    Changed background picture , updated css resposivness; deteted unwanted scripts from calc.js

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,68 +1,6 @@
 /*--Testing power button to be checked--*/
-var addition = require("scripts/calc.js");
+var addition = require("../scripts/calc.js");
 
-// var onButton = document.querySelector('input[name = "onButton"]:checked');
-//     if("onButton" != null){
-//         it("should be equal false", function(){
-//                     expect("on").toBe("false");
-//                 });
-//         };
-
-
-
-// /*--Testinng play function ---*/
-
-// describe("play function test ",function(){
-//     beforeEach(function(){
-//     });
-//   it("winner should not be true",function(){
-//     expect('winner').not.toBe(true);
-//   });
-//   it("good should be true",function(){
-//     expect('good').toBe(true);
-//   });
-// });
-
-
-
-/*refactoring play function
-function playFunction(soundid, targetid, classname) {
-  if (sound) {
-    var audio = document.getElementById(soundid); 
-    audio.play(); 
-  }
-  sound = true;
-  $('#' + targetid).addClass(classname);
-}
-
-
-So we get :
-
-if (gameTurn) { //When it's still games turn 
-        $("#on").prop("disabled", true);
-        clearColor();
-        setTimeout(() => {
-            if (order[flash] == 1) { //Function responsible for flashing top left button 
-                playFunction('sound1', 'topleft', 'litTopLeft');
-                 audio.play();
-            }
-            if (order[flash] == 2) { //Function responsible for flashing top right button 
-                playFunction('sound2', 'topright', 'litTopRight');
-                 audio.play();
-            }
-            if (order[flash] == 3) { //Function responsible for flashing bottom leftbutton
-                playFunction('sound3', 'bottomleft', 'litBottomLeft');
-                 audio.play();
-            }
-            if (order[flash] == 4) { //Function responsible for flashing bottom right button
-                playFunction('sound4', 'bottomright', 'litBottomRight');
-                 audio.play();
-            }
-            flash++;
-        }, 700);
-    }
-}
-*/
 
 describe("Calculator", function() {
     describe("Addition function", function() {
