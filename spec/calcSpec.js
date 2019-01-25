19b4451b253606172e6030c95b9c768fa67f3a57
Pawel P <pawelpelech@gmail.com>
2019-01-25 20:33:14 +0000

    second jasmine test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,7 @@ const utils = require("..scripts/calc.js");
 describe("Testing a random array of numbers",function(){
   it ("generateRandomNumbers should return a set of a random numbers", function(){
       random1 = utils.generateRandomNumbers(5);
+      random2 = utils.generateRandomNumbers(5);
+      expect(random1).not.toBe(random2);
   })
 });
